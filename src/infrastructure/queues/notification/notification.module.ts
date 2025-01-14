4e70b24577f1c0d0ddf7c2a771dd55f4e11fce63
Grisha BeeWeb <grisha.hovhanyann@gmail.com>
2025-01-14 09:57:26 +0400

    feat: setup bull board

--- src/infrastructure/queues/notification/notification.module.ts
+++ src/infrastructure/queues/notification/notification.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common'
 import { BullModule } from '@nestjs/bullmq'
+import { BullBoardModule } from '@bull-board/nestjs'
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
 
 import { QueueNames } from '../queue.enum'
 import { NotificationQueueService } from './notification.service'
 import { NotificationProcessor } from './notification.processor'
 
 @Module({
-  imports: [BullModule.registerQueue({ name: QueueNames.notification })],
+  imports: [
+    BullModule.registerQueue({ name: QueueNames.notification }),
+    BullBoardModule.forFeature({ name: QueueNames.notification, adapter: BullMQAdapter })
+  ],
   providers: [NotificationQueueService, NotificationProcessor],
   exports: [NotificationQueueService]
 })
--- src/infrastructure/queues/queue.module.ts
+++ src/infrastructure/queues/queue.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common'
 import { BullModule } from '@nestjs/bullmq'
+import { ExpressAdapter } from '@bull-board/express'
+import { BullBoardModule } from '@bull-board/nestjs'
+import basicAuth from 'express-basic-auth'
 
+import { envService } from '@app/common'
 import { REDIS_CONFIG } from '@app/database'
 import { NotificationQueueModule } from './notification/notification.module'
 
@@ -19,6 +23,18 @@ const queueModules = [NotificationQueueModule]
   imports: [
     BullModule.forRoot({
       connection: { url: REDIS_CONFIG.url }
+    }),
+    BullBoardModule.forRoot({
+      route: '/admin/bull-board',
+      adapter: ExpressAdapter,
+      middleware: [
+        basicAuth({
+          challenge: true,
+          users: {
+            [envService.getEnvString('BULL_ADMIN_USERNAME')]: envService.getEnvString('BULL_ADMIN_PASSWORD')
+          }
+        })
+      ]
     }),
     ...queueModules
   ],
